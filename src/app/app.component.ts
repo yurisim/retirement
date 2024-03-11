02091b3b562b8dc43de0acfbc9dbbc6540b8c47c
Yura Sim <yuriosim@gmail.com>
2024-03-10 22:41:31 -0500

    Update app component imports and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'retirement-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'retirement';
+  title = 'CMSgt Tracy L. Massey Retirement';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    // routes for home
+    // { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+    // // routes for bios
+    // { path: 'bio', loadChildren: () => import('./bio/bio.module').then(m => m.BioModule) },
+    // // routes for events
+    // { path: 'event', loadChildren: () => import('./event/event.module').then(m => m.EventModule) },
+    // // routes for photos
+    // { path: 'photo', loadChildren: () => import('./photo/photo.module').then(m => m.PhotoModule) },
+];
