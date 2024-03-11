f93f271b3e1784415fe72bb01474f56eb40d69a5
Yura Sim <yuriosim@gmail.com>
2024-03-11 05:54:48 -0500

    Update app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatIconModule, MatTooltipModule, MatButtonModule],
+  imports: [RouterModule, MatIconModule, MatTooltipModule, MatButtonModule, MatMenuModule],
   selector: 'retirement-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
