dab4e1efb46371b894759da2103d0a95138a025d
Yura Sim <yuriosim@gmail.com>
2024-04-01 03:05:01 -0500

    Update styles and content in various components

--- src/app/bios/bios.component.ts
+++ src/app/bios/bios.component.ts
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 export class BiosComponent {
   personData = [
     {
+      officialImage: "assets/images/CVOP.png",
       images: ["assets/images/abm-senior.png"],
       name: "Colonel Kenneth J. Voigt, Jr.",
       bio: [
@@ -25,6 +26,14 @@ export class BiosComponent {
         "ENDURING FREEDOM",
         "IRAQI FREEDOM",
         "SOUTHERN WATCH"
+      ],
+      education: [
+        "1999 Bachelor of Science, Electrical Engineering, Norwich University, Northfield, Vermont",
+        "2004 Master of Human Relations, University of Oklahoma, Norman, Oklahoma",
+        "2005 Squadron Officer School, Maxwell AFB, Alabama",
+        "2007 USAF Weapons School, Nellis AFB, Nev.",
+        "2011 Master of Arts in National Security & Strategic Studies, U.S. Naval War College",
+        "2017 Master of Science in Joint Campaign Planning, Joint Advanced Warfighting School"
       ],
       awards: [
         "Legion of Merit",
@@ -44,15 +53,62 @@ export class BiosComponent {
         { rank: "Lt Col", date: "01 Dec 2013" },
         { rank: "Col", date: "01 Jan 2020" }
 
+      ],
+      flightData: [
+        "Rating: Senior Air Battle Manager",
+        "Flight hours: 2,240 - 459 Combat/104 Combat Support",
+        "Aircraft Flown: E-3B/C"
       ]
     },
     {
+      officialImage: "assets/images/CTMPhoto.png",
       images: ["assets/images/c2.png", "assets/images/wdir.png"],
       name: "CMSgt Tracy L. Massey",
       bio: [
         "Chief Master Sergeant Tracy Massey is the 552d Air Control Group Superintendent, Tinker AFB, Oklahoma. She is the Senior Enlisted Advisor, to the Commander for over 900 Airmen, comprised of diverse Air Force Specialty Codes at four local and Geographically Separated Units. She provides operations, maintenance, logistics and training support to two combat-coded Air Control Squadrons, one Air Control Networks Squadron and one Operations Support Squadron, effectively ensuring Command and Control Battle Management Operations to Combatant Commanders worldwide.",
         "Chief Massey entered the Air Force in May 1994. After gaining technical knowledge at the Alaska NORAD Region Air Operations Center, Chief Massey was selected to attend Weapons Director training. Upon graduation, she deployed in support of Operation SOUTHERN WATCH. As a member of her unit’s convoy from Kuwait to Baghdad International Airport, she aided in the set-up of the first mobile ground radar unit in Iraq. Later, she managed, tracked and sourced approximately 1,000 deployments for Tactical Control Systems, Air Battle Managers, Tactical Control Party and Space taskings. As Operations Superintendent, she led 32 Airmen, working with Canadian Forces, to accomplish their NORAD mission. In her last assignment, she was the Headquarters Air Force Command and Control Battle Management Operations Career Field Manager."],
-      assignments: ["To Do"],
+      assignments: [
+        {
+          "time": "July 2021 - Present",
+          "description": "Superintendent, 552 Air Control Group, Tinker AFB, OK."
+        },
+        {
+          "time": "August 2018 - July 2021",
+          "description": "Headquarters Air Force C2 Battle Management Operations CFM, Pentagon, D.C."
+        },
+        {
+          "time": "August 2017 - August 2018",
+          "description": "HQ AETC C2 Battle Management Operations, Functional Area Manager, Joint Base San Antonio Randolph, TX"
+        },
+        {
+          "time": "October 2013 - August 2017",
+          "description": "Superintendent, Detachment 2, First Air Force, CFB North Bay, Ontario, Canada"
+        },
+        {
+          "time": "December 2009 - July 2013",
+          "description": "Superintendent, AEF Operations Scheduling Section, HQ Air Force Personnel Center, Randolph Air Force Base, TX"
+        },
+        {
+          "time": "July 2006 - December 2009",
+          "description": "Noncommissioned Officer in Charge, Operations Flights, 607 Air Control Squadron, Luke Air Force Base, AZ"
+        },
+        {
+          "time": "May 2002 - July 2006",
+          "description": "Noncommissioned Officer in Charge, Weapons and Tactics, 728 Air Control Squadron, Eglin Air Force Base, FL"
+        },
+        {
+          "time": "May 2001 - May 2002",
+          "description": "Security Manager, 621 Air Control Squadron, Osan Air Base, Republic of South Korea"
+        },
+        {
+          "time": "January 1998 - May 2001",
+          "description": "Operations Training Specialist, 74 Air Control Squadron, Langley Air Force Base, VA"
+        },
+        {
+          "time": "August 1994 - December 1997",
+          "description": "Noncommissioned Officer in Charge, Simulation/Exercise Training, 611 Air Control Squadron, Elmendorf Air Force Base, AK"
+        }
+      ],
       awards: ["Legion of Merit", "Meritorious Service Medal", "Air Force Commendation Medal", "Air Force Achievement Medal"],
       achievements: [
         { year: 2008, description: "Distinguished Graduate, Airmen Leadership School" },
